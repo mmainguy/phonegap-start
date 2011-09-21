d8a50c1ebb8d6f6a8da657b3baed7594952c3163
Michael Mainguy <mike.mainguy@gmail.com>
2011-09-21 06:55:22 -0500

    styled buttons, simplified js

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,39 +21,38 @@ run(function () {
             my_str = "";
             if (data.curr_punch != null) {
                 my_str = toHours(d.valueOf() - data.curr_punch);
-
                 my_str = " (" + my_str + ")"
-
+                x$('#start_button').addClass('pressed');
             }
             x$('#title_bar').html('inner', d.toLocaleTimeString() + my_str);
         }, 1000);
 
     })();
 
-    x$('#start_button').on('click', function() {
-        setTime('start')
+    x$('.timer_button').on('click', function() {
+        setTime(this)
     });
 
-    x$('#stop_button').on('click', function() {
-        setTime('stop')
-    });
     function toHours(time_interval) {
         return (time_interval / (60000 * 60)).toFixed(2);
     }
 
-    function setTime(start_or_stop) {
+    function setTime(trigger) {
+        x$('.app_button').removeClass('pressed');
+        x$('#'+trigger.id).addClass('pressed');
+
+
         var curr = new Date();
         if (data.curr_punch != null) {
             data.data.push({start: data.curr_punch.valueOf(), stop: curr.valueOf(), duration: curr.valueOf() - data.curr_punch });
             data.curr_punch = null;
         } else {
-            if (start_or_stop == 'start') {
+            if (trigger.id == 'start_button') {
                 data.curr_punch = curr.valueOf();
             } else {
                 data.curr_punch = null;
             }
-
-        }
+    }
         store.save(data, function(ret) {
             data = ret;
         });
@@ -80,7 +79,14 @@ run(function () {
             curr_punch: null
         };
 
-    })
+    });
+
+    x$('.button').on('touchstart',function(){
+        x$('#'+this.id).addClass('pressed');
+    });
+    x$('.button').on('touchend',function(){
+            x$('#'+this.id).removeClass('pressed');
+    });
 
     function reset_view() {
         loadTable();
@@ -89,14 +95,16 @@ run(function () {
 
     function loadTable() {
         x$('#tracking-table tbody tr').html('remove');
-
+        total = 0.0;
         data.data.forEach(function(row) {
+            total += row.stop - row.start;
             x$('#tracking-table tbody').html('bottom', '<tr><td>' +
                     new Date(row.start).toLocaleTimeString() + '</td><td>' +
                     new Date(row.stop).toLocaleTimeString()
                     + '</td><td>' +
                     toHours(row.stop - row.start) + '</td></tr>');
         });
+        x$('#tracking-table tbody').html('bottom','<tr><td></td><td></td><td>'+toHours(total)+'</td></tr>');
     }
 
 });
